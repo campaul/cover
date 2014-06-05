e29df675fa5867d491dac110c53745b66b622e81
Cameron Paul <cpaul37@gmail.com>
2014-06-04 18:11:36 -0700

    Setup tox

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='Cover',
+    version='0.0.0',
+    description='Album Art Manager',
+    author='Cameron Paul',
+    author_email='cpaul37@gmail.com',
+    url='https://github.com/campaul/cover',
+    packages=['cover'],
+)
