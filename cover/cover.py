8f262644525865f0b3d8f4dd355b376a41afdf97
Garrett Pennell <garrettpennell@gmail.com>
2014-06-06 16:10:13 -0500

    Removed extraneous line

--- cover/cover.py
+++ cover/cover.py
@@ -1,7 +1,6 @@
 from cover.providers import google
 
 def get_album_art(artist, album):
-    query = ' '.join([artist, album, 'cover art']).replace(' ', '%20')
     return sorted(google.load(artist, album), key=score_album_art).pop()
 
 def score_album_art(image):
