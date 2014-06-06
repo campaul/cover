b63cbf0b03edc75a84d29c6b6ec40ffd4521285d
Garrett Pennell <garrettpennell@gmail.com>
2014-06-06 16:04:46 -0500

    Moved query building to providers instead of cover.py

--- cover/cover.py
+++ cover/cover.py
@@ -2,7 +2,7 @@ from cover.providers import google
 
 def get_album_art(artist, album):
     query = ' '.join([artist, album, 'cover art']).replace(' ', '%20')
-    return sorted(google.load(query), key=score_album_art).pop()
+    return sorted(google.load(artist, album), key=score_album_art).pop()
 
 def score_album_art(image):
     return image.width if image.width == image.height else 0
