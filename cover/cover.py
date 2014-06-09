242e4659cf4e7942e7e5485c22b5e3a266e6f551
Cameron Paul <cpaul37@gmail.com>
2014-06-08 17:14:43 -0700

    Combine google and musicbrainz results

--- cover/cover.py
+++ cover/cover.py
@@ -2,7 +2,13 @@ from cover.providers import google
 from cover.providers import musicbrainz
 
 def get_album_art(artist, album):
-    return sorted(musicbrainz.load(artist, album), key=score_album_art).pop()
+    musicbrainz_results = get_album_art_from_provider(artist, album, musicbrainz)
+    google_results = get_album_art_from_provider(artist, album, google)
+
+    return (musicbrainz_results + google_results).pop()
+
+def get_album_art_from_provider(artist, album, provider):
+    return sorted(provider.load(artist, album)[:3], key=score_album_art)
 
 def score_album_art(image):
     return image.width if image.width == image.height else 0
